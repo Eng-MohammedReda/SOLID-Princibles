3f22c1ea733ac6749b53fe62337c2b5d2fa10659
Mohammed Reda <46918252+Eng-MohammedReda@users.noreply.github.com>
2020-06-26 13:45:58 +0300

    Add files via upload
    SOLID Principles demo application used in my videos on youtube play list https://youtu.be/-hbWDYRSjqk

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using solidConsoleDemo.ISP;
+using solidConsoleDemo.LSP;
+using solidConsoleDemo.OCP;
+
+namespace solidConsoleDemo
+{
+    public class Item { 
+        public string code;
+        public double price;
+        public double quantity; 
+    }
+    public class Stock
+    {
+        public static List<Item> items = new List<Item> {
+                                 new Item{code="123", price=10, quantity=10},
+                                 new Item{code="456", price=12, quantity=2},
+                                 new Item{code="789", price=14, quantity=1}
+        };
+
+        public static bool IsAvailableQuantity(Item item){
+            if(item.quantity <items.Where(x => x.code == item.code).FirstOrDefault().quantity ){
+                return true;
+            }else {return false;}
+        }
+    }
+
+    public class ShopingCart
+    {
+        List<Item> cartItems = new List<Item>();
+
+        public void RemoveFromCart(Item item)
+        {
+            if (cartItems.Contains(item)) cartItems.Remove(item);
+        }
+        public bool Add(Item item)
+        {
+            // check available quantity
+           
+            if (Stock.IsAvailableQuantity(item))
+            {
+                cartItems.Add(item); return true;
+            }
+            else { return false; }
+        }
+        
+        public int GetItemsCount(){return cartItems.Count;}
+         
+    }
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           DIP.Notification notification =
+            new  DIP.Notification(new DIP.WebMail());
+          
+           notification.mailType = new DIP.Gmail();
+           notification.SendMail();
+           
+           Console.ReadKey();
+        }
+    }
+}
